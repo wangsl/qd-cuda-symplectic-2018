b28ad622bd2e585c1e561ce4dfc05b4ccdbcd8a5
wangsl <wangsl2001@gmail.com>
2020-07-12 20:56:28 -0400

    before switch matlab 2020

--- common-cuda/symplecticUtils.h
+++ common-cuda/symplecticUtils.h
@@ -12,7 +12,7 @@ namespace SymplecticUtils {
   
   // from J. Chem. Phys. 104, 7099 (1996)
   const double m6n4_a[6] = { 0.2167979108466032, -0.0283101143283301, 0.3901418904713324,
-			     -0.2414087476423302,  0.5908564573813148, 0.0719226032714098 };
+			    -0.2414087476423302,  0.5908564573813148, 0.0719226032714098 };
   
   const double m6n4_b[6] = { 0.0719226032714098,  0.5908564573813148, -0.2414087476423302, 
 			     0.3901418904713324, -0.0283101143283301,  0.2167979108466032 };
@@ -25,14 +25,12 @@ namespace SymplecticUtils {
   
   inline const double *a()
   { return MatlabData::si_coefficients() ?
-      (const double *) MatlabData::si_coefficients()->a :
-      m6n4_a;
+      (const double *) MatlabData::si_coefficients()->a : m6n4_a;
   }
   
   inline const double *b()
   { return MatlabData::si_coefficients() ?
-      (const double *) MatlabData::si_coefficients()->b :
-      m6n4_b;
+      (const double *) MatlabData::si_coefficients()->b : m6n4_b;
   }
 }
 
